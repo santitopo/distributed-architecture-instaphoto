cda620f9da462eccb4a08b1d8e14a46f35ea0496
santitopo <santiagotopolansky@gmail.com>
2020-11-18 14:50:08 -0300

    AdministrativeClient added

---
+++ ObligatorioTopolanskyNajson/AdministrativeClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AdministrativeClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositories();
+        }
+        
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://localhost:44359/weatherforecast");
+
+            var msg = await stringTask;
+            Console.Write(msg);
+        }
+        
+    }
+    
+    public class Repository
+    {
+        public string name { get; set; }
+    }
+}
